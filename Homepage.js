e56311bbb3625158a3738cc9d1a6153075760deb
Johnmar-LCJS <johnmararellano@yahoo.com>
2022-01-26 08:57:35 +0800

    Project Commit

--- Homepage.js
+++ Homepage.js
@@ -1,7 +1,12 @@
 const body = document.getElementsByTagName("body")[0];
+const arrow = document.getElementById("rightArrow");
+const message = document.getElementById("ycsn")
 
 body.addEventListener("click", scroll);
 
 function scroll() {
 	body.style.overflowY = "auto";
+	arrow.style.display = "block"
+	message.textContent = "(you can scroll now)"
+
 }
